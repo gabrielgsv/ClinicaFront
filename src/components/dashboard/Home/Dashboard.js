106b904b779f7cb1d32398ee62cd289248fa5321
gabrielgsv <gabriel.soares.vilela@gmail.com>
2018-09-03 20:47:34 -0300

    teste deploy

--- src/components/dashboard/Home/Dashboard.js
+++ src/components/dashboard/Home/Dashboard.js
@@ -42,7 +42,7 @@ class Dashboard extends Component {
 
   componentDidMount = () => {
     axios
-      .get(`/api/recuperartoken`)
+      .get(`https://clini-api-staging.herokuapp.com/api/recuperartoken`)
       .then(response => {
         this.setState({
           dadosUsuario: {
@@ -62,7 +62,7 @@ class Dashboard extends Component {
 
   validarTokenSessao() {
     axios
-      .get(`/api/validartoken`, {
+      .get(`https://clini-api-staging.herokuapp.com/api/validartoken`, {
         withCredentials: true,
         headers: {
           Authorization: "Bearer " + this.state.tokenUser
