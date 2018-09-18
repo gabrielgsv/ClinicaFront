be8ec591c934b1e138f4fd75cb7f1f13421ee9a9
jvcampos <jjvcamposs@gmail.com>
2018-09-17 21:21:31 -0300

    teste

--- src/components/dashboard/Paciente/NovaConsulta.js
+++ src/components/dashboard/Paciente/NovaConsulta.js
@@ -359,20 +359,18 @@ class NovaConsulta extends Component {
         data: dateString,
         loading: true
       },
-    })
-    this.buscarHorasDisponiveis()
+    },
+      axios.get(`${API_ROOT}/api/medico/horariosdisponiveis/${this.state.agendamento.data}/${this.state.agendamento.codigomedico}`)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    )
+    console.log(this.state.agendamento)
     const horas = [7, 8, 9, 10, 11, 13, 14, 15, 16, 17] //exemplo get não funcionando
     this.setState({ horas })
-  }
-
-  buscarHorasDisponiveis() {
-    axios.get(`${API_ROOT}/api/medico/horariosdisponiveis/${this.state.agendamento.data}/${this.state.agendamento.codigomedico}`)
-    .then(response => {
-      console.log(response)
-    })
-    .catch(err => {
-      console.log(err)
-    })
   }
 
   render() {
